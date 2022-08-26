72968b5f8f17cb41513f6702ef055df8462671d2
朱献康 <zhuxiankang@zhuxiankangdeMacBook-Pro.local>
2022-08-26 09:12:59 +0800

    feat: hello demo

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+// 引入标准库
+#include <iostream>
+
+int main() {
+    int a = 1;
+    int b = 2;
+    // cout 是 std 命名空间下的对象，用于标准输出
+    // 类似于 Web 前端中的 console.log("Hello, world")
+    std::cout << a+b <<  std::endl;
+}
