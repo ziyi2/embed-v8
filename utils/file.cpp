d4b6d537cc01d4283edc2c1214b5faaf508aeeeb
ziyi2 <18768107826@163.com>
2023-04-07 09:26:00 +0800

    feat: 测试 V8 上下文

--- utils/file.cpp
+++ utils/file.cpp
@@ -4,17 +4,19 @@
 
 using namespace std;
 
-// C++ 读写示例：https://cplusplus.com/doc/tutorial/files/，查看最后一个示例 Binary files
+// C++ 读写示例：https://cplusplus.com/doc/tutorial/files/
+// 查看最后一个示例 Binary files
 
 char* readJavaScriptFile(const char* fileName) {
     
     char* code;
 
     // 创建一个 ifstream，并打开名为 fileName 的文件
-    // fileName 可以是 string 类型，也可以是一个指向 c 风格字符串的指针
+    // fileName 可以是 string 类型，也可以是一个指向字符串存储地址的指针
 
     // ios::in：打开文件用于读取
     // ios::ate：文件打开后定位到文件末尾
+    // 这里的 | 是一个位运算符，表明上述两个操作都要执行
     ifstream file(fileName, ios::in|ios::ate);
 
     if(file.is_open()) {
@@ -24,7 +26,7 @@ char* readJavaScriptFile(const char* fileName) {
         code = new char[size];
         // 重新定位到文件开头
         file.seekg (0, ios::beg);
-        // 读取文件的全部内容（读取的内容可以不是全部，因此会有第二个参数 size 指定读取的文件大小）
+        // 读取文件的全部内容（通过 size 指定读取的文件大小）
         file.read (code, size);
         // 关闭文件
         file.close();
