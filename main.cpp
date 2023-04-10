3dd88e4eaceeed525e29d2b90f95d15bfc563d6f
ziyi2 <18768107826@163.com>
2023-04-10 09:44:43 +0800

    feat: 测试 V8 上下文

--- main.cpp
+++ main.cpp
@@ -29,6 +29,10 @@ int main(int argc, char* argv[]) {
     // Create a stack-allocated handle scope.
     v8::HandleScope handle_scope(isolate);
 
+    /**
+     1. 创建一个上下文，执行 micro1.js 和 micro2.js
+    */
+
     {
 
       // Create a new context.
@@ -79,8 +83,41 @@ int main(int argc, char* argv[]) {
         printf("%s\n", *utf8);
       }
     }
+    
+    /**
+     2. 创建两个上下文，分别执行 micro1.js 和 micro2.js
+    */
 
-     {
+    {
+      // Create a new context.
+      // 创建一个新的上下文
+      v8::Local<v8::Context> context = v8::Context::New(isolate);
+
+      // Enter the context for compiling and running the hello world script.
+      // 进入新的上下文执行 JS 代码
+      v8::Context::Scope context_scope(context);
+
+      // 读取当前 main.cpp 同级目录下的 micro1.js 文件
+      const char* code = readJavaScriptFile("micro1.js");
+
+      // Create a string containing the JavaScript source code.
+      v8::Local<v8::String> source =
+          v8::String::NewFromUtf8(isolate, code).ToLocalChecked();
+
+      // Compile the source code.
+      v8::Local<v8::Script> script =
+          v8::Script::Compile(context, source).ToLocalChecked();
+
+      // Run the script to get the result.
+      v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
+
+      // Convert the result to an UTF8 string and print it.
+      v8::String::Utf8Value utf8(isolate, result);
+      printf("%s\n", *utf8);
+    }
+
+
+    {
       // Create a new context.
       // 创建一个新的上下文
       v8::Local<v8::Context> context = v8::Context::New(isolate);
